cf4f4d4fe396b653c734d47040927cee98ee38f7
Cecília Romão <ceciliaromao@gmail.com>
2024-07-02 21:20:27 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
+app.get('/oi', function (req, res) {
+    res.send('Olá, Mundo')
+  })
+
 app.listen(3000)
